72ca3f8f47d59b3eb15eaefdb5dc56dc1070acaa
Thommy <48998059+YourBoiThommy@users.noreply.github.com>
2022-05-04 08:58:44 +0200

    doshit

--- Streamline_Placement/streamline_placement/testing_streamline_placement.py
+++ Streamline_Placement/streamline_placement/testing_streamline_placement.py
@@ -18,13 +18,13 @@ init_point = np.array([[0.,0.,0.]])
 #u_list, v_list, w_list, mesh = mesh_creation(nx, ny, nz, datafile) #look a few lines above
 mesh = pv.UniformGrid(dims=(nx,ny,nz), spacing=(1,1,1), origin=(0,0,0))
 integration_direction = "forward"
-initial_step_length = 0.5
+initial_step_length = 1
 step_unit = "cl" #'cell length'
-min_step_length = 0.5
+min_step_length = 1
 max_steps = 2000
 terminal_speed = 0
-dsep = 5.0
-radius = 10.0
+dsep = 0.5
+radius = 1
 u_list, v_list, w_list = np.ones((1000,1)), np.ones((1000,1)), np.ones((1000,1))
 mesh['vectors'] = np.ones((1000,3))
 n_seed_points = 4
