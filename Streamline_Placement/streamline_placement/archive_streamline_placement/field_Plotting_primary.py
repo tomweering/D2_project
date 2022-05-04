33ae97ef64e0050f24c1a24b89690fdabf487e84
Thommy <48998059+YourBoiThommy@users.noreply.github.com>
2022-05-04 22:24:03 +0200

    Update field_Plotting_primary.py

--- Streamline_Placement/streamline_placement/archive_streamline_placement/field_Plotting_primary.py
+++ Streamline_Placement/streamline_placement/archive_streamline_placement/field_Plotting_primary.py
@@ -10,23 +10,33 @@ def field_plotter(dimensions,datafile):
     #data file is csv file with columns: x y z scalar
 
     mesh = pv.UniformGrid(
-        dims=dimensions
+        dims=dimensions,
+        origin=(1.0, 1.0, 1.0)
     )
     direction_vectors, scalars = scaled_vector_processing(datafile)
     mesh['Vectors'] = direction_vectors
     mesh['Scalars'] = scalars
-    #print(mesh)
+
+    #boundary = clipped.decimate_boundary().extract_all_edges()
+    #mesh.set_active_scalars('Vectors')
+    #streamlines, src = mesh.streamlines(
+    #    return_source=True, source_radius=5, source_center=(59, 27, 2)
+    #)
+
     arrows = mesh.glyph(orient="Vectors", scale="Scalars")
-    #print(arrows)
     #(xmin, xmax, ymin, ymax, ,zmin zmax)
     bounds = [59, 79, 27, 44, 2, 22]
     clipped = arrows.clip_box(bounds)
+    boundary = mesh.decimate_boundary().extract_all_edges()
     pl = pv.Plotter()
+    #pl.add_mesh(streamlines.tube(radius=0.2), lighting=False)
+    #pl.add_mesh(src)
+    pl.add_mesh(boundary, color="grey", opacity=0.25)
+    #pl.camera_position = [(10, 9.5, -43), (87.0, 73.5, 123.0), (-0.5, -0.7, 0.5)]
     pl.add_mesh(arrows,  style='wireframe', color='blue', label='Input')
     pl.add_mesh(clipped, style='wireframe', color='red', label='Clipped')
     pl.show()
-    #print(arrows)
-    #print(clipped)
+
     return mesh, arrows, clipped
 
 mesh, arrows, clipped = field_plotter(dimensions,"output_vfield.csv")
