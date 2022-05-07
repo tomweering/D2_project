fd286587cb7d2d89ded52f298230105169b10699
rafalberes <rafal3.beres@gmail.com>
2022-05-07 15:46:24 +0200

    New guiding streamlines added in guiding_str3.json Streamline_invert functionality added in gstreamlines_modify.py

--- VectorFilteringOrientation/gstreamlines_modify.py
+++ VectorFilteringOrientation/gstreamlines_modify.py
@@ -3,21 +3,39 @@ import numpy as np
 from compas.geometry import Polyline
 import matplotlib.pyplot as plt
 
-streamlines = compas.json_load('guiding_str2.json')
+streamlines = compas.json_load('guiding_str3.json')
 N = len(streamlines)
 span = [0, N] #range of streamlines that will be printed
+
+str_to_invert = [35, 19, 17, 18, 41, 14, 13, 15, 12, 11, 8, 9, 16, 10, 39, 28, 29, 27, 26, 25]
+str_to_delete = [10, 36]
+
+if_modify = True
+
 
 
 def unit_length(streamlines, length): #Divide polylines into unit-long segments
     str_unified = streamlines
-    counter = 0
+    str_unified_points = []
+
     for i, streamline in enumerate(streamlines[0:120]):
         str_unified[i] = Polyline(streamline.divide_by_length(length, strict=False))
-        counter += len(str_unified[i])
+        str_unified_points.append(str_unified[i].points)
+        #counter += len(str_unified[i])
 
-    return str_unified, counter
 
-def poly_to_vectors(str_unified):   #Converts a polyline into a set of vectors tangential to that polyline
+    return str_unified, str_unified_points
+
+
+def modify(invert, delete,  streamlines):
+    for i in invert:
+        streamlines[i].reverse()
+    # for i in delete:
+    #     del streamlines[i]
+    #del streamlines[delete]
+    return streamlines
+
+def poly_to_vectors(str_unified_points):   #Converts a polyline into a set of vectors tangential to that polyline
     str_vectorsx = []
     str_vectorsy = []
     str_vectorsz = []
@@ -25,8 +43,9 @@ def poly_to_vectors(str_unified):   #Converts a polyline into a set of vectors t
     str_locationsy = []
     str_locationsz = []
 
-    for k, polyline in enumerate(str_unified):
-        points = polyline.points
+    # for k, polyline in enumerate(str_unified):
+    #     points = polyline.points
+    for k, points in enumerate(str_unified_points):
         single_loc = []
         single_vec = []
 
@@ -60,12 +79,17 @@ def poly_to_vectors(str_unified):   #Converts a polyline into a set of vectors t
     str_vectors = [str_vectorsx, str_vectorsy, str_vectorsz]
     return str_vectors, str_locations
 
-str_unified, counter = unit_length(streamlines, 1)
+str_unified, str_points = unit_length(streamlines, 1)
 
-str_vectors, str_locations = poly_to_vectors(str_unified)
+if if_modify:
+    str_points = modify(str_to_invert, str_to_delete, str_points)
+
+str_vectors, str_locations = poly_to_vectors(str_points)
 
 fig = plt.figure(dpi=100)
 ax = fig.add_subplot(projection='3d')
+plt.axis('off')
+plt.grid(b=None)
 for i in range(len(str_vectors[0])):
 
     locations_x = str_locations[0][i]
@@ -76,9 +100,10 @@ for i in range(len(str_vectors[0])):
     vectors_y = str_vectors[1][i]
     vectors_z = str_vectors[2][i]
 
-    ax.scatter(locations_x[::2], locations_y[::2], locations_z[::2])
+    #ax.scatter(locations_x[::2], locations_y[::2], locations_z[::2])
     #ax.annotate(i, (locations_x[::10], locations_y[::10], locations_z[::10]))
     ax.text(locations_x[0], locations_y[0], locations_z[0], '%s' % (str(i)))
+    ax.text(locations_x[-1], locations_y[-1], locations_z[-1], '%s' % (str(i)))
     ax.quiver(locations_x[::10], locations_y[::10], locations_z[::10], vectors_x[::10], vectors_y[::10], vectors_z[::10], length = 6.0)
 #plt.legend()
 plt.show()
