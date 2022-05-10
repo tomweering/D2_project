5f23ee6d059f2210827c816e1c54c8089695a404
Thommy <48998059+YourBoiThommy@users.noreply.github.com>
2022-05-10 15:48:05 +0200

    Update clipped_testing_streamline_placement.py

--- Streamline_Placement/streamline_placement/clipped_testing_streamline_placement.py
+++ Streamline_Placement/streamline_placement/clipped_testing_streamline_placement.py
@@ -2,54 +2,36 @@ import numpy as np
 import pyvista as pv
 
 from main_streamline_placement import streamline_placement
-#from functions_streamline_placement import interpolator2
 from functions_mesh_creation import mesh_creation
-#from inputs_streamline_placement import *
 
 """--------------------INPUTS: MESH CREATION------------------------"""
-
 
 nx = 95
 ny = 59
 nz = 36
 
-#datafile = "Test_Case2.csv"
+# datafile = "Test_Case2.csv"
 
-datafileXmZM = "output_vfield.csv"
+datafileXmZM = "output_new_orientation.csv"
 datafileXMZm = "field_vect_scaledByDensity.csv"
-#output_vfield.csv              #Pyvista
-#field_vect_scaledByDensity.csv #Scipy
+# output_vfield.csv              #Pyvista
+# field_vect_scaledByDensity.csv #Scipy
 
 
 """--------------------INPUTS: STREAMLINE PLACEMENT------------------------"""
+pv.set_plot_theme("document")
 
-init_point = np.array([[0.,0.,0.]])
+mesh_pyvista, mesh_scipy, u_list, v_list, w_list = mesh_creation(nx, ny, nz, datafileXmZM, datafileXMZm)
 
-mesh_pyvista, mesh_scipy, u_list, v_list, w_list = mesh_creation(nx, ny, nz, datafileXmZM, datafileXMZm) #look a few lines above
-print(mesh_pyvista)
-#mesh = pv.UniformGrid(dims=(nx,ny,nz), spacing=(1,1,1), origin=(0,0,0))
+init_point = [47.5, 29.5, 18.0]
 
-bounds = [59, 79, 27, 44, 2, 22]
-clipped = mesh_pyvista.clip_box(bounds)
-mesh_pyvista = clipped
-print(clipped['vectors'])
-integration_direction = "forward"
-initial_step_length = 1
-step_unit = "cl" #'cell length'
-min_step_length = 1
-max_steps = 2000
-terminal_speed = 0
-dsep = 0.25
-radius = 0.5
-#u_list, v_list, w_list = np.ones((1000,1)), np.ones((1000,1)), np.ones((1000,1))
-#mesh['vectors'] = np.ones((1000,3))
-n_seed_points = 4
+streamlines_bracket = mesh_pyvista.streamlines(vectors="vectors", n_points=25000, source_radius=50,
+                                               source_center=init_point, terminal_speed=0.0)
+p = pv.Plotter()
+p.add_mesh(streamlines_bracket.tube(radius=0.1))
+p.add_mesh(mesh_pyvista.outline())
+p.show()
 
 """--------------------CALLING FUNCTIONS------------------------"""
 
-
-
-
-streamline_placement(init_point,  mesh_pyvista, mesh_scipy, u_list, v_list, w_list, integration_direction, initial_step_length, step_unit, min_step_length, max_steps, terminal_speed, dsep, radius, n_seed_points)
-
-
+# streamline_placement(init_point,  mesh_pyvista, mesh_scipy, u_list, v_list, w_list, integration_direction, initial_step_length, step_unit, min_step_length, max_steps, terminal_speed, dsep, radius, n_seed_points)
