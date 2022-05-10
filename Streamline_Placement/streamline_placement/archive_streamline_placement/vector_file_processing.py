f5fb2a291da87417224ea70f1d7cd93211331759
Thommy <48998059+YourBoiThommy@users.noreply.github.com>
2022-05-10 15:45:12 +0200

    Update vector_file_processing.py

--- Streamline_Placement/streamline_placement/archive_streamline_placement/vector_file_processing.py
+++ Streamline_Placement/streamline_placement/archive_streamline_placement/vector_file_processing.py
@@ -1,17 +1,15 @@
 import numpy as np
 
 
-
 def scaled_vector_processing(datafile):
-    #process a csv file formed of scaled vectors associated with a mesh to return an array of unit vectors with a single scalar value describing the scaling
+    # process a csv file formed of scaled vectors associated with a mesh to return an array of unit vectors with a
+    # single scalar value describing the scaling
     with open(datafile) as f:
-        array = np.loadtxt(f,delimiter=",")
+        array = np.loadtxt(f, delimiter=",")
 
-    scalars = np.linalg.norm(array,axis=1)
-    direction_vectors = (array.T/scalars).T
-    #print(scalars)
+    scalars = np.linalg.norm(array, axis=1)
+    direction_vectors = (array.T / scalars).T
     return direction_vectors, scalars
 
-direction_vectors, scalars = scaled_vector_processing("output_vfield_without_zeros.csv")
-        
-    
+
+direction_vectors, scalars = scaled_vector_processing("output_new_unoriented2.csv")
