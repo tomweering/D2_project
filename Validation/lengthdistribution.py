24d281697bf9bc8b7bc78bbe30e4acc7cbdad55b
silviotopper <silviotopper1@gmail.com>
2022-05-13 16:50:59 +0200

    Update lengthdistribution.py

--- Validation/lengthdistribution.py
+++ Validation/lengthdistribution.py
@@ -1,29 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-print("The AVERAGE streamline lenght is", np.average(list_of_streamlines), "mm")
-print("The LONGEST streamline lenght is", max(streamline_lengths), "mm")
-print("The SHORTEST streamline lenght is", min(streamlines_lengths), "mm")
+print("The AVERAGE streamline lenght is", np.average(a_list_of_streamline_lengths), "mm")
+print("The LONGEST streamline lenght is", max(a_list_of_streamline_lengths), "mm")
+print("The SHORTEST streamline lenght is", min(a_list_of_streamline_lengths), "mm")
 
-streamline_lengths = [78, 94, 28, 57, 48, 89, 91, 47, 47, 84, 55]
 distribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-maximum = max(streamline_lengths)
-minimum = min(streamline_lengths)
+tenpercentdistribution = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+maximum = max(a_list_of_streamline_lengths)
+minimum = min(a_list_of_streamline_lengths)
 step = (maximum - minimum) / 10
+smallstep = step / 10
+
 silvio = []
 for i in range(10):
     silvio.append(maximum - (step * i))
-print(silvio)
-for value in streamline_lengths:
+
+tom = []
+for i in range(10):
+    tom.append(maximum - (smallstep * i))
+
+for value in a_list_of_streamline_lengths:
     for i in range(10):
         if value >= maximum - (step * (i+1)):
             distribution[i] += 1
             break
 
+for value in a_list_of_streamline_lengths:
+    for i in range(10):
+        if value >= maximum - (smallstep * (i+1)):
+            tenpercentdistribution[i] += 1
+            break
 
-
+plt.subplot(2, 1, 1)
 plt.bar(silvio, distribution)
-plt.xlabel('Fraction Of Max Length Streamline', fontsize=5)
-plt.ylabel('# of Streamlines', fontsize=5)
+plt.xlabel('Fraction Of Max Length Streamline', fontsize=10)
+plt.ylabel('# of Streamlines', fontsize=10)
 plt.title('Streamline Length Distribution')
+
+plt.subplot(2, 1, 2)
+plt.bar(tom, tenpercentdistribution)
+plt.xlabel('Fraction Of Max Length Streamline', fontsize=10)
+plt.ylabel('# of Streamlines', fontsize=10)
+plt.title('Streamline Length Distribution')
+
 plt.show()
