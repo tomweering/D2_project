c220edcb4878eae4b364019c3a53135dfe9457a9
rafalberes <rafal3.beres@gmail.com>
2022-05-18 13:35:30 +0200

    Fixed pyvista streamlines printing

--- Stacking/streamlines_to_surface_fit.py
+++ Stacking/streamlines_to_surface_fit.py
@@ -4,11 +4,10 @@ import scipy.spatial as spatial
 import matplotlib.pyplot as plt
 # from stacking2 import points
 # from stacking2 import streamlines_points, points_xyz
-points = np.load('surface_numpypoints.npy')
-points_xyz = np.load('streamlines_numpypoints.npy')
-idx = np.load('streamlines_index.npy')
+surface_points = np.load('surface_numpypoints.npy')
+points_xyz = np.load('hr_eq_spaced.npy')
+idx = np.load('hr_eq_spaced_idx.npy')
 idx = idx.astype('int32')
-print(idx)
 
 # surf = cloud.delaunay_2d(alpha=1.0)
 # surf.plot(cpos="xy", show_edges=True)
@@ -23,12 +22,12 @@ def lines_from_points(points):
     poly.lines = cells
     return poly
 
-cloud = pv.PolyData(points)
+cloud = pv.PolyData(surface_points)
 #cloud2.plot(point_size=10)
 # surf = cloud.delaunay_2d(alpha=4.0)
 # surf.plot(cpos="xy", show_edges=True)
 
-tree = spatial.KDTree(points)
+tree = spatial.KDTree(surface_points)
 dd, ii = tree.query(points_xyz, workers=-1)
 # print(len(points))
 # print(len(points_xyz))
@@ -50,39 +49,19 @@ sorted_idx = idx_short[mean.argsort()]
 p = pv.Plotter()
 selected_str = []
 print(sorted_idx)
-for i in sorted_idx[0:50]:
-    print(i)
+for i in sorted_idx[0:500]:
     selected_str = np.append(selected_str, points_xyz[np.where(idx == i)])
-    print(points_xyz[np.where(idx == i)])
     line = lines_from_points(points_xyz[np.where(idx == i)])
     line["scalars"] = np.arange(line.n_points)
     tube = line.tube(radius=0.1)
     p.add_mesh(tube, color="tan", show_edges=True)
-print(selected_str)
+
+# for i in range(500):
+#     line = lines_from_points(points_xyz[np.where(idx == i)])
+#     line["scalars"] = np.arange(line.n_points)
+#     tube = line.tube(radius=0.1)
+#     p.add_mesh(tube, color="tan", show_edges=True)
 # selected_cloud = pv.PolyData(selected_str)
 # selected_cloud.plot(point_size=10)
 
-
-
-
-#p.show()
-
-
-# print(dd)
-# print(ii)
-# print(len(dd), len(ii))
-
-# closest_points = points_xyz[ii]
-# cloud_close = pv.PolyData(closest_points)
-# cloud_close.plot(point_size=5)
-
-# p = pv.Plotter()
-# p.add_mesh(h0, smooth_shading=True)
-# p.show_grid()
-# p.show()
-
-# fig = plt.figure(figsize=(10, 10))
-# ax = fig.gca(projection='3d')
-# ax.scatter(points[:, 0], points[:, 1], c=points[:, 2])
-#
-# plt.show()
+p.show()
